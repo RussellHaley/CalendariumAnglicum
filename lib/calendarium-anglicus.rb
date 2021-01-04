172a0a9425d4c5abb385955fd2c8ba4bd9beae0d
Jakub Pavlik <severus@post.cz>
2021-01-04 21:47:38 +0100

    require custom CelebrationFactory and Dates, make them work

--- lib/calendarium-anglicus.rb
+++ lib/calendarium-anglicus.rb
@@ -6,6 +6,8 @@ end
 
 require 'calendarium-anglicus/version'
 require 'calendarium-anglicus/data'
+require 'calendarium-anglicus/temporale/celebration_factory'
+require 'calendarium-anglicus/temporale/dates'
 require 'calendarium-anglicus/temporale'
 
 # convenience shortcut
